918b8edb726aeb32158356cf9a4a09722d49fe48
Chris Miles <cw.miles121@gmail.com>
2022-02-24 12:49:54 -0500

    Menu function progress

--- src/bankofmiles/Account.java
+++ src/bankofmiles/Account.java
@@ -16,7 +16,7 @@ public class Account {
         customerID = cid;
     }
 
-//    Methods for Depositing Money
+//    Method for Depositing Money
     void deposit(int amount) {
         if (amount > 0) {
             balance = balance + amount;
@@ -24,6 +24,61 @@ public class Account {
         }
     }
 
-    
+//    Method for Withdrawing Money
+    void withdraw(int amount) {
+        if(amount > 0) {
+            balance = balance - amount;
+            previousTransaction = -amount;
+        }
+    }
+
+//    Method showing previous transaction
+    void getPreviousTransaction() {
+        if(previousTransaction > 0) {
+            System.out.println("Deposited: " + previousTransaction);
+        } else if (previousTransaction < 0) {
+            System.out.println("Withdrawn: " + Math.abs(previousTransaction));
+        } else {
+            System.out.println("No transaction occurred");
+        }
+    }
+
+//    Method calculating interest of current funds after a number of years
+    void calculateInterest(int years) {
+        double interestRate = .0185;
+        double newBalance = (balance * interestRate * years) + balance;
+        System.out.println("The current interest rate is " + (100 * interestRate) + "%");
+        System.out.println("After " + years + " years, your balance will be: " + newBalance);
+    }
+
+//    Method showing menu
+    void showMenu() {
+        char option = '\0';
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome, " + customerName + "!");
+        System.out.println("Your ID is: " + customerID);
+        System.out.println();
+        System.out.println("What would you like to do?");
+        System.out.println();
+        System.out.println("A. Check your balance");
+        System.out.println("B. Make a deposit");
+        System.out.println("C. make a withdrawal");
+        System.out.println("D. View previous transaction");
+        System.out.println("E. Calculate interest");
+        System.out.println("F. Exit");
+
+        do {
+            System.out.println();
+            System.out.println("Enter an option: ");
+            char option1 = scanner.next().charAt(0);
+            option = Character.toUpperCase(option1);
+            System.out.println();
+
+            switch (option) {
+                
+            }
+        }
+
+    }
 
 }
